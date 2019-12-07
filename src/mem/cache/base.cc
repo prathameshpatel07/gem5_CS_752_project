922271997d6671648a975a9ff1d2d9cf94680f07
Ishan Vijay Yelurwar <yelurwar@tux-55.cae.wisc.edu>
2019-12-06 21:42:13 -0600

    Commented check for size
    Change-Id: Ie96edeb4e07f3af677b75f889a6751b59c924dc1

--- src/mem/cache/base.cc
+++ src/mem/cache/base.cc
@@ -958,8 +958,8 @@ BaseCache::satisfyRequest(PacketPtr pkt, CacheBlk *blk, bool, bool)
         // all read responses have a data payload
         assert(pkt->hasRespData());
         //If read request from load copy the cache block to the buffer
-        if (pkt->cmd == MemCmd::ReadReq && isDCache
-        && coalescing_buffer.size() <= SIZE) {
+        if (pkt->cmd == MemCmd::ReadReq && isDCache) {
+        //&& coalescing_buffer.size() <= SIZE) { //Check for size
             uint8_t *d = (uint8_t *)malloc(64);
             memcpy(d, blk->data, 64);
             uint64_t mask = ~((1 << 6) - 1);
